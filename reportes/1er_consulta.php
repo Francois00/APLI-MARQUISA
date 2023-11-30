7b66971b7137d6a484cd2bb53741acf62eb422b2
Don6abo <gabopolo6@gmail.com>
2023-11-30 10:09:20 -0500

    s

--- reportes/1er_consulta.php
+++ reportes/1er_consulta.php
@@ -1,7 +1,3 @@
-
-
-?>
-<?php
 include '../funciones.php';
 
 csrf();
@@ -13,97 +9,52 @@ $error = false;
 $config = include '../config.php';
 
 try {
+
   $dsn = 'mysql:host=' . $config['db']['host'] . ';dbname=' . $config['db']['name'];
   $conexion = new PDO($dsn, $config['db']['user'], $config['db']['pass'], $config['db']['options']);
 
-  
+  // Agregar la sentencia `SET NAMES utf8mb4` para asegurar que los datos se devuelvan en la codificación UTF-8.
+  $conexion->exec("SET NAMES utf8mb4");
 
+  // Consulta SQL
   $consultaSQL = "SELECT cabecera_orden_compra.obra, proveedor.nom, proveedor.dir
-  FROM cabecera_orden_compra
-  INNER JOIN proveedor ON cabecera_orden_compra.ruc_prov = proveedor.ruc
-  ORDER BY cabecera_orden_compra.obra";
+                    FROM cabecera_orden_compra
+                    INNER JOIN proveedor ON cabecera_orden_compra.ruc_prov = proveedor.ruc
+                    ORDER BY cabecera_orden_compra.obra;";
 
-    // Preparar y ejecutar la consulta
-    $sentencia = $conexion->prepare($consultaSQL);
-    $sentencia->execute();
+  // Preparar y ejecutar la consulta
+  $sentencia = $conexion->prepare($consultaSQL);
+  $sentencia->execute();
+
+  // Obtener los resultados
+  $resultados = $sentencia->fetchAll(PDO::FETCH_ASSOC);
 
 } catch (PDOException $error) {
-  $error = $error->getMessage();
+
+  // Manejar errores de la base de datos
+  echo "Error: " . $error->getMessage();
+
 }
-?>
 
-<?php include "../templates/header.php"; ?>
+// Mostrar los resultados
+if (isset($resultados) && count($resultados) > 0) {
 
-<?php
-if ($error) {
-  ?>
-  <div class="container mt-2">
-    <div class="row">
-      <div class="col-md-12">
-        <div class="alert alert-danger" role="alert">
-          <?= $error ?>
-        </div>
-      </div>
-    </div>
-  </div>
-  <?php
+  // Agregar un encabezado a la tabla
+  echo "<table border='1'><tr><th>Obra</th><th>Proveedor</th><th>Dirección</th></tr>";
+
+  // Recorrer los resultados y mostrarlos en la tabla
+  foreach ($resultados as $fila) {
+    echo "<tr><td>" . $fila['obra'] . "</td><td>" . $fila['nom'] . "</td><td>" . $fila['dir'] . "</td></tr>";
+  }
+
+  // Cerrar la tabla
+  echo "</table>";
+
+} else {
+
+  // Mostrar un mensaje si no hay resultados
+  echo "<p>No hay resultados para mostrar.</p>";
+
 }
+
 ?>
-
-<div class="container">
-  <div class="row">
-    <div class="col-md-12">
-      <a href="crear_prov.php" class="btn btn-primary mt-4">Crear Proveedor</a>
-      <hr>
-      <form method="post" class="form-inline">
-        <div class="form-group mr-3">
-          <input type="text" id="ruc" name="ruc" placeholder="Buscar por RUC" class="form-control">
-        </div>
-        <input name="csrf" type="hidden" value="<?php echo escapar($_SESSION['csrf']); ?>">
-        <button type="submit" name="submit" class="btn btn-primary">Ver resultados</button>
-      </form>
-    </div>
-  </div>
-</div>
-
-<div class="container">
-  <div class="row">
-    <div class="col-md-12">
-      <h2 class="mt-3">
-        <?= $titulo ?>
-      </h2>
-      <table class="table">
-        <thead>
-          <tr>
-            <th>OBRA</th>
-            <th>PROVEEDOR NOMBRE</th>
-            <th>PROVEEDOR DIRECCION</th>
-          </tr>
-        </thead>
-        <tbody>
-          <?php
-          if ($proveedor && $sentencia->rowCount() > 0) {
-            foreach ($proveedor as $fila) {
-              ?>
-              <tr>
-                <td>
-                  <?php echo escapar($fila["ruc"]); ?>
-                </td>
-                <td>
-                  <?php echo escapar($fila["nom"]); ?>
-                </td>
-                <td>
-                  <?php echo escapar($fila["dir"]); ?>
-                </td>
-              </tr>
-              <?php
-            }
-          }
-          ?>
-        <tbody>
-      </table>
-    </div>
-  </div>
-</div>
-
-<?php include "../templates/footer.php"; ?>
